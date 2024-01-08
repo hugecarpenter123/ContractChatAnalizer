21c97a1c5a3287a6cf74cb71580315b07d9a7271
lenovo <arkkupi85@gmail.com>
2024-01-08 17:04:27 +0100

    added static-to-dynamic results

--- ApiManager.py
+++ ApiManager.py
@@ -12,20 +12,20 @@ class APIManagerZad1:
 
     @staticmethod
     def cohere_api(smartcontract_content, test_result):
-        body = APIManagerZad3._generate_api_content(smartcontract_content, test_result)
-        response = cohere.Client(APIManagerZad3.cohere_api_key).chat(body, model="command", temperature=0.9)
+        body = APIManagerStaticToDynamic._generate_api_content(smartcontract_content, test_result)
+        response = cohere.Client(APIManagerStaticToDynamic.cohere_api_key).chat(body, model="command", temperature=0.9)
         answer = response.text
         print(answer)
         return answer
 
     @staticmethod
     def llama_api(smartcontract_content, test_result):
-        content = APIManagerZad3._generate_api_content(smartcontract_content, test_result)
+        content = APIManagerStaticToDynamic._generate_api_content(smartcontract_content, test_result)
         api_request_json = {
             "messages": [{"role": "user", "content": content}],
             "stream": False,
         }
-        response = LlamaAPI(APIManagerZad3.llama_api_key).run(api_request_json)
+        response = LlamaAPI(APIManagerStaticToDynamic.llama_api_key).run(api_request_json)
         choices = response.json()["choices"]
         for choice in choices:
             message = choice.get("message", {})
@@ -41,7 +41,7 @@ class APIManagerZad1:
 
     @staticmethod
     def textcortext_api(smartcontract_content, test_result):
-        content = APIManagerZad3._generate_api_content(smartcontract_content, test_result)
+        content = APIManagerStaticToDynamic._generate_api_content(smartcontract_content, test_result)
         url = "https://api.textcortex.com/v1/codes"
         headers = {
             "Authorization": "Bearer gAAAAABlfG5BBC3rNLSjTFfpjwo293OPUgfJDm_w-v6vF2f64wOjel5KF3vW5scXN-Pt5hCLQNFpCCMWEyoJNIOb_dY5PGWDpkHXAx4eM61eUb6Z6NEtbdSK3uAYt-70TzjdpUjJW9yh"
@@ -82,20 +82,20 @@ class APIManagerZad2:
 
     @staticmethod
     def cohere_api(smartcontract_content, test_result):
-        body = APIManagerZad3._generate_api_content(smartcontract_content, test_result)
-        response = cohere.Client(APIManagerZad3.cohere_api_key).chat(body, model="command", temperature=0.9)
+        body = APIManagerStaticToDynamic._generate_api_content(smartcontract_content, test_result)
+        response = cohere.Client(APIManagerStaticToDynamic.cohere_api_key).chat(body, model="command", temperature=0.9)
         answer = response.text
         print(answer)
         return answer
 
     @staticmethod
     def llama_api(smartcontract_content, test_result):
-        content = APIManagerZad3._generate_api_content(smartcontract_content, test_result)
+        content = APIManagerStaticToDynamic._generate_api_content(smartcontract_content, test_result)
         api_request_json = {
             "messages": [{"role": "user", "content": content}],
             "stream": False,
         }
-        response = LlamaAPI(APIManagerZad3.llama_api_key).run(api_request_json)
+        response = LlamaAPI(APIManagerStaticToDynamic.llama_api_key).run(api_request_json)
         choices = response.json()["choices"]
         for choice in choices:
             message = choice.get("message", {})
@@ -111,7 +111,7 @@ class APIManagerZad2:
 
     @staticmethod
     def textcortext_api(smartcontract_content, test_result):
-        content = APIManagerZad3._generate_api_content(smartcontract_content, test_result)
+        content = APIManagerStaticToDynamic._generate_api_content(smartcontract_content, test_result)
         url = "https://api.textcortex.com/v1/codes"
         headers = {
             "Authorization": "Bearer gAAAAABlfG5BBC3rNLSjTFfpjwo293OPUgfJDm_w-v6vF2f64wOjel5KF3vW5scXN-Pt5hCLQNFpCCMWEyoJNIOb_dY5PGWDpkHXAx4eM61eUb6Z6NEtbdSK3uAYt-70TzjdpUjJW9yh"
@@ -152,20 +152,20 @@ class APIManagerZad3:
 
     @staticmethod
     def cohere_api(smartcontract_content, test_result):
-        body = APIManagerZad3._generate_api_content(smartcontract_content, test_result)
-        response = cohere.Client(APIManagerZad3.cohere_api_key).chat(body, model="command", temperature=0.9)
+        body = APIManagerStaticToDynamic._generate_api_content(smartcontract_content, test_result)
+        response = cohere.Client(APIManagerStaticToDynamic.cohere_api_key).chat(body, model="command", temperature=0.9)
         answer = response.text
         print(answer)
         return answer
 
     @staticmethod
     def llama_api(smartcontract_content, test_result):
-        content = APIManagerZad3._generate_api_content(smartcontract_content, test_result)
+        content = APIManagerStaticToDynamic._generate_api_content(smartcontract_content, test_result)
         api_request_json = {
             "messages": [{"role": "user", "content": content}],
             "stream": False,
         }
-        response = LlamaAPI(APIManagerZad3.llama_api_key).run(api_request_json)
+        response = LlamaAPI(APIManagerStaticToDynamic.llama_api_key).run(api_request_json)
         choices = response.json()["choices"]
         for choice in choices:
             message = choice.get("message", {})
@@ -181,7 +181,7 @@ class APIManagerZad3:
 
     @staticmethod
     def textcortext_api(smartcontract_content, test_result):
-        content = APIManagerZad3._generate_api_content(smartcontract_content, test_result)
+        content = APIManagerStaticToDynamic._generate_api_content(smartcontract_content, test_result)
         url = "https://api.textcortex.com/v1/codes"
         headers = {
             "Authorization": "Bearer gAAAAABlfG5BBC3rNLSjTFfpjwo293OPUgfJDm_w-v6vF2f64wOjel5KF3vW5scXN-Pt5hCLQNFpCCMWEyoJNIOb_dY5PGWDpkHXAx4eM61eUb6Z6NEtbdSK3uAYt-70TzjdpUjJW9yh"
@@ -214,3 +214,81 @@ class APIManagerZad3:
     @staticmethod
     def _generate_api_content(smartcontract_content, test_result):
         return f"Rozmowa będzie prowadzona w języku polskim. Mając ten smartkontrakt: `{smartcontract_content}`, oraz ten wynik testów tego smartkontraktu: `{test_result}`, zaproponuj poprawiony smartkontrakt."
+
+
+class APIManagerStaticToDynamic:
+    static_testing_method = "Solhint"
+    dynamic_testing_method = "Manticore"
+    cohere_api_key = 'nasXJmj9GyZF0hH7LsS3CwXoVqrEqc4QlolOAM7B'
+    llama_api_key = 'LL-4Upqf2Vo34LciTSHPNG44yNuByGyvoxXATFaZ5CaktJWp42OgkHcO4VQROLP2LLT'
+
+    @staticmethod
+    def cohere_api(smartcontract_content, test_result):
+        print("COHERE api start...")
+        body = APIManagerStaticToDynamic._generate_api_content(smartcontract_content, test_result)
+        response = cohere.Client(APIManagerStaticToDynamic.cohere_api_key).chat(body, model="command", temperature=0.9)
+        answer = response.text
+        # print(answer)
+        print("COHERE api end.")
+        return answer
+
+    @staticmethod
+    def llama_api(smartcontract_content, test_result):
+        print("LAMA api start...")
+        content = APIManagerStaticToDynamic._generate_api_content(smartcontract_content, test_result)
+        api_request_json = {
+            "messages": [{"role": "user", "content": content}],
+            "stream": False,
+        }
+        response = LlamaAPI(APIManagerStaticToDynamic.llama_api_key).run(api_request_json)
+        choices = response.json()["choices"]
+        for choice in choices:
+            message = choice.get("message", {})
+            content = message.get("content")
+
+            if content:
+                # print(f"\n{'-' * 20}\n", content, end=f"\n{'-' * 20}\n")
+                print("LAMA api end.")
+                return content
+            else:
+                print("Brak odpowiedzi tekstowej od \"Lama\"")
+                return "Brak odpowiedzi tekstowej od \"Lama\""
+        return "Coś poszło nie tak..."
+
+    @staticmethod
+    def textcortext_api(smartcontract_content, test_result):
+        print("TEXTCORTEXT api start...")
+        content = APIManagerStaticToDynamic._generate_api_content(smartcontract_content, test_result)
+        url = "https://api.textcortex.com/v1/codes"
+        headers = {
+            "Authorization": "Bearer gAAAAABlfG5BBC3rNLSjTFfpjwo293OPUgfJDm_w-v6vF2f64wOjel5KF3vW5scXN-Pt5hCLQNFpCCMWEyoJNIOb_dY5PGWDpkHXAx4eM61eUb6Z6NEtbdSK3uAYt-70TzjdpUjJW9yh"
+        }
+        data = {
+            "max_tokens": 512,
+            "mode": "python",
+            "model": "icortex-1",
+            "n": 1,
+            "temperature": 0,
+            "text": content
+        }
+
+        response = requests.post(url, headers=headers, json=data)
+
+        if response.status_code == 200:
+            parsed_response = response.json()
+            outputs = parsed_response.get('data', {}).get('outputs', [])
+            if outputs:
+                text_output = outputs[0].get('text')
+                print("Odpowiedź textcortext: ", text_output)
+                print("TEXTCORTEXT api end.")
+                return text_output
+            else:
+                print("Brak danych tekstowych w odpowiedzi API.")
+                return None
+        else:
+            print("Błąd w żądaniu:", response.status_code)
+            return "Błąd podczas otrzymywania odpowiedzi czatu \"textcortext\""
+
+    @staticmethod
+    def _generate_api_content(smartcontract_content, test_result):
+        return f"Mając ten smartkontrakt: `{smartcontract_content}`, oraz wynik testów tego smartkontraktu przy użyciu narzędzia {APIManagerStaticToDynamic.static_testing_method}: `{test_result}`, zaproponuj testy dynamiczne przy użyciu narzędzia {APIManagerStaticToDynamic.dynamic_testing_method}"
--- main.py
+++ main.py
@@ -2,29 +2,62 @@ import os
 import codecs
 from ApiManager import APIManagerZad1
 from ApiManager import APIManagerZad2
-from ApiManager import APIManagerZad3
+from ApiManager import APIManagerStaticToDynamic
 
-folder_path = './res/smartcontracts'
-output_folder_path = './res/output'
+contracts_folder_path = './res/smartcontracts'
+static_tests_results_folder_path = './res/smartcontracts_static_tests_results'
+dynamic_tests_results_folder_path = './res/smartcontracts_dynamic_tests_results'
 
-def custom_operation(content):
-    print(content)
-    return "some random content"
+static_to_dynamic_output_folder_path = './res/output/static_to_dynamic'
 
-sol_files = [f for f in os.listdir(folder_path) if f.endswith('.sol')]
+
+
+def custom_operation(contract_content, static_test_result, dynamic_test_result):
+    print("=" * 100)
+    print(contract_content)
+    print(static_test_result)
+    print(dynamic_test_result)
+    print("=" * 100, "\n")
+
+
+sol_files = [f for f in os.listdir(contracts_folder_path) if f.endswith('.sol')]
+static_tests_results_files = [f for f in os.listdir(static_tests_results_folder_path)]
+dynamic_tests_results_files = [f for f in os.listdir(dynamic_tests_results_folder_path)]
 
 for file_name in sol_files:
-    file_path = os.path.join(folder_path, file_name)
+    contract_path = os.path.join(contracts_folder_path, file_name)
+    static_test_path = os.path.join(static_tests_results_folder_path, file_name.replace('.sol', '_solhint.txt'))
+    dynamic_test_path = os.path.join(dynamic_tests_results_folder_path, file_name.replace('.sol', '_manticore.txt'))
 
-    with codecs.open(file_path, 'r', encoding='utf-8') as file:
-        file_content = file.read()
+    if os.path.isfile(static_test_path) and os.path.isfile(dynamic_test_path):
+        with codecs.open(contract_path, 'r', encoding='utf-8') as contract_file, \
+                codecs.open(static_test_path, 'r', encoding='utf-8') as static_test_file, \
+                codecs.open(dynamic_test_path, 'r', encoding='utf-8') as dynamic_test_file:
+            contract_content = contract_file.read()
+            static_test_result = static_test_file.read()
+            dynamic_test_result = dynamic_test_file.read()
 
-    result = custom_operation(file_content)
+            lama_result = APIManagerStaticToDynamic.llama_api(contract_content, static_test_result)
+            cohere_result = APIManagerStaticToDynamic.cohere_api(contract_content,
+                                                                 static_test_result)
+            textcortext_result = APIManagerStaticToDynamic.textcortext_api(contract_content,
+                                                                           static_test_result)
 
-    # Tworzenie nazwy pliku wyjściowego
-    output_file_name = file_name.replace('.sol', '_output.txt')
-    output_file_path = os.path.join(output_folder_path, output_file_name)
+            # Tworzenie nazwy pliku wyjściowego
+            lama_output_file_name = file_name.replace('.sol', '_lama.txt')
+            lama_output_file_path = os.path.join(static_to_dynamic_output_folder_path, lama_output_file_name)
 
-    # Zapis wyniku do pliku wyjściowego
-    with codecs.open(output_file_path, 'w', encoding='utf-8') as output_file:
-        output_file.write(result)
+            cohere_output_file_name = file_name.replace('.sol', '_cohere.txt')
+            cohere_output_file_path = os.path.join(static_to_dynamic_output_folder_path, cohere_output_file_name)
+
+            textCortext_output_file_name = file_name.replace('.sol', '_textcortext.txt')
+            textCortext_output_file_path = os.path.join(static_to_dynamic_output_folder_path, textCortext_output_file_name)
+
+            # Zapis wyniku do pliku wyjściowego
+            with codecs.open(lama_output_file_path, 'w', encoding='utf-8') as lama_output_file, \
+                    codecs.open(cohere_output_file_path, 'w', encoding='utf-8') as cohere_output_file, \
+                    codecs.open(textCortext_output_file_path, 'w', encoding='utf-8') as textCortext_output_file:
+
+                lama_output_file.write(lama_result)
+                cohere_output_file.write(cohere_result)
+                textCortext_output_file.write(textcortext_result)
